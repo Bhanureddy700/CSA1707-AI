9da2265dc2191fa801eed68f6ca780e26fba4b4a
Bhanureddy700 <113880826+Bhanureddy700@users.noreply.github.com>
2023-11-30 08:48:36 +0530

    Add files via upload

---
+++ medical diagnosis.pl
@@ -0,0 +1,21 @@
+symptom(john, fever).
+symptom(john, cough).
+symptom(jane, headache).
+symptom(jane, nausea).
+
+disease(fever, flu).
+disease(cough, cold).
+disease(headache, stress).
+disease(nausea, flu).
+
+% Rule to diagnose a disease based on symptoms
+diagnose(Person, Disease) :-
+    symptom(Person, Symptom),
+    disease(Symptom, Disease).
+
+% Example queries
+% What disease does John have?
+% ?- diagnose(john, Disease).
+%
+% What disease does Jane have?
+% ?- diagnose(jane, Disease).
